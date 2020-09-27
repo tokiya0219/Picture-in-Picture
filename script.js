5d8688340fd359dddff2f26461b86a90b21b9461
tokiya0219 <53889976+tokiya0219@users.noreply.github.com>
2020-09-27 18:54:25 +0900

    inital commit

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+//Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try{
+        const meadiaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = meadiaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(err) {
+        console.log('whoops, error here:', err);
+    }
+}
+
+button.addEventListener('click', async () => {
+    button.disabled = true;
+
+    await videoElement.requestPictureInPicture();
+
+    button.disabled = false;
+});
+
+selectMediaStream();
